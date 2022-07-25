f4aff9d4ba09fcb4b6b7c658f35646fe76d5851c
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-07-25 16:48:21 -0400

    feat: estilização da Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,33 @@
+import { Play } from "phosphor-react"
+import { CountdownContainer, FormContainer, Taskinput, StartCountdownButton, HomeContainer, Separator, MinutesAmountInput } from "./styles"
+
 export const Home = () => {
   return (
-    <h1>Home</h1>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task"> Vou focar em</label>
+          <Taskinput type="text" id="task" placeholder="ex: estudar inglês..." />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput type="number" id="minutesAmount"/>
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} weight="bold" />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   )
 }
