ecc2d3a2f7b3be605069ba9596e60831e1fab8ab
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-07-25 18:07:51 -0400

    refactor: tratativas no input

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,10 +7,17 @@ export const Home = () => {
       <form action="">
         <FormContainer>
           <label htmlFor="task"> Vou focar em</label>
-          <Taskinput type="text" id="task" placeholder="ex: estudar inglês..." />
+          <Taskinput type="text" id="task" list="task-suggestions" placeholder="ex: estudar inglês..." />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount"/>
+          <MinutesAmountInput type="number" step={5} min={5} max={60} id="minutesAmount"/>
 
           <span>minutos.</span>
         </FormContainer>
