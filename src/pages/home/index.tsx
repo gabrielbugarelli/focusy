87155e3f1548fb1bba6c265a66db373d2031c8ed
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-08-03 19:37:40 -0400

    feat: implementação do countdown

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -15,22 +16,53 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export const Home = () => {
+  const [ cycles, setCycles ] = useState<Cycle[]>([]);
+  const [ activeCycleId, setActiveCycleId ] = useState<string | null>(null);
+  const [ amountSecondsPassed, setAmountSecondsPassed ] = useState(0);
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
-      minutesAmount: 0
-    }
-  });
+      minutesAmount: 0,
+    },
+  })
+
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = Math.floor(currentSeconds % 60);
+
+  const minutes = String(minutesAmount).padStart(2, '0');
+  const seconds = String(secondsAmount).padStart(2, '0');
+
+  const task = watch('task');
+  const isSubmitDisabled = !task;
 
   const handleCreateNewCycle = (data: NewCycleFormData) => {
-    console.log(data);
+
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id: id,
+      task: data.task,
+      minutesAmount: data.minutesAmount
+    }
+
+    setCycles( state => [...state, newCycle] );
+    setActiveCycleId(id);
+
     reset();
   };
-
-  const task = watch('task'); 
-  const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
@@ -40,17 +72,9 @@ export const Home = () => {
           <Taskinput 
             type="text" 
             id="task" 
-            list="task-suggestions" 
             placeholder="ex: estudar inglês..."
             {...register('task')}
           />
-
-          <datalist id="task-suggestions">
-            <option value="Projeto 1" />
-            <option value="Projeto 2" />
-            <option value="Projeto 3" />
-            <option value="Projeto 4" />
-          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput type="number" step={5} 
@@ -64,11 +88,11 @@ export const Home = () => {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
