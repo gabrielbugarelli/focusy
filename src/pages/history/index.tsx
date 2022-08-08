a2fa030d255a17fcbb891aa7389e1906f03cec4a
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-08-08 16:19:48 -0400

    feat: implementação da listagem do histórico

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -21,23 +21,26 @@ export const History = () => {
           </thead>
 
           <tbody>
-            <tr>
-              <td>Estudar NodeJS</td>
-              <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
-              <td> 
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Estudar Clean Architecture</td>
-              <td>20 minutos</td>
-              <td>Há cerca de 2 meses</td>
-              <td> 
-                <Status statusColor="yellow">Em progresso</Status>
-              </td>
-            </tr>
+            {cycles.map(cycle => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos </td>
+                  <td>{cycle.startDate.toISOString()}</td>
+                  <td> 
+                    {cycle.finishedDate && (
+                      <Status statusColor="green"> Concluído </Status>
+                    )}
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red"> Interrompido </Status>
+                    )}
+                    {(!cycle.finishedDate && !cycle.interruptedDate) && (
+                      <Status statusColor="yellow"> Em andamento </Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </HistoryList>
