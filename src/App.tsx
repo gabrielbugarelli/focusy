6289b45c061d653e725c810f73850106bf950ea8
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-08-08 15:05:54 -0400

    refactor: melhorias no contexto entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/theme/default"
 import { GlobalStyle } from "./styles/global"
 import { Router } from "./routes"
+import { CyclesContextProvider } from "./contexts/CyclesContext"
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Router />
-      <GlobalStyle />
+      <CyclesContextProvider>
+        <Router />
+        <GlobalStyle />
+      </CyclesContextProvider>
     </ThemeProvider>
   )
 }
--- src/pages/home/components/NewCycleForm/index.tsx
+++ src/pages/home/components/NewCycleForm/index.tsx
@@ -1,7 +1,7 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 import { useContext } from 'react'
-import { CyclesContext } from '../..'
 import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 export function NewCycleForm() {
   const { activeCycle } = useContext(CyclesContext)
@@ -9,11 +9,11 @@ export function NewCycleForm() {
 
   return (
     <FormContainer>
-      <label htmlFor="task">Vou trabalhar em</label>
+      <label htmlFor="task">Vou focar em</label>
       <TaskInput
         id="task"
         list="task-suggestions"
-        placeholder="Dê um nome para o seu projeto"
+        placeholder="ex: estudar inglês..."
         disabled={!!activeCycle}
         {...register('task')}
       />
